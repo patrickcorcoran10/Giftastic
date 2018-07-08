34420f67e0d3bf377c56667a09818b0541cee32a
Patty C <patrickcorcoran10@gmail.com>
2018-07-07 21:46:14 -0500

    homework final

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -3,69 +3,86 @@ var animals = ["lion", "tiger", "bear"];
 function renderButtons() {
     $("#animalButtons").empty();
 
-    for (var i = 0; i < animals.length; i++) {
-        var a = $("<button>");
-        a.addClass("animal");
-        a.attr("id", animals[i]);
-        a.attr("data-name", animals[i]);
-        a.text(animals[i]);
-        $("#animalButtons").append(a);
-    }
-};
-
-$("#animalbuttons").on("click", function(event) {
-    event.preventDefault();
-    var animalB = $("#lion", "#tiger", "#bear").val();
-    
-    
-
-});
-
-$("#add-animal").on("click", function(event){
-    event.preventDefault();
-    var animalA = $("#submit").val().trim();
-    animals.push(animalA);
-    
-
-    renderButtons();
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalA + "&api_key=hnT5lnVzXNifAuxi8qoo3j0wa8XhIQip&limit=10";
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-      var results = response.image_original_url;
-
-      for (var i = 0; i <results.length; i++) {
-          
-      
-      var imageURL = results.gif;
-      var animalImage = $("<img>");
-      animalImage.attr("src", imageURL);
-      animalImage.attr("alt", "animal image");
-
-      $("#animalGif").prepend(animalImage);
-      };
-      $("#submit").empty("");
+            for (var i = 0; i < animals.length; i++) {
+                var a = $("<button>");
+                a.addClass("animal");
+                a.attr("id", animals[i]);
+                a.attr("data-name", animals[i]);
+                a.text(animals[i]);
+                $("#animalButtons").append(a);
+            }
+            $("#animalbuttons").on("click", function(event) {
+                event.preventDefault();
+                var animalB = this;
+                console.log(animalB)
+                var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalsB + "&api_key=jEyoNNAhVcbsV0o1H5kflHtad50NbpDE&limit=10";
+            
+                $.ajax({
+                url: queryURL,
+                method: "GET"
+                }).then(function(response) {
+                console.log(response);
+                
+            
+                for (var i = 0; i <response.data.length; i++) {
+                    
+                
+                var imageURL = response.data[i].images.original_still.url;
+                var animalImage = $("<img>");
+                animalImage.attr("src", imageURL);
+                animalImage.attr("alt", "animal image");
+            
+                $("#animalGif").prepend(animalImage);
+                };
+                $("#submit").empty("");
+                });
+        });
+            $("#add-animal").on("click", function(event){
+                event.preventDefault();
+                var animalA = $("#submit").val().trim();
+                animals.push(animalA);
+                
+            
+                renderButtons();
+                var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalA + "&api_key=jEyoNNAhVcbsV0o1H5kflHtad50NbpDE&limit=10";
+            
+                $.ajax({
+                url: queryURL,
+                method: "GET"
+                }).then(function(response) {
+                console.log(response);
+                
+            
+                for (var i = 0; i <response.data.length; i++) {
+                    
+                
+                var imageURL = response.data[i].images.original_still.url;
+                var animalImage = $("<img>");
+                animalImage.attr("src", imageURL);
+                animalImage.attr("alt", "animal image");
+                animalImage.attr("data-state", "animate");
+                animalImage.attr("class", "gif");
+                animalImage.attr("data-still", response.data[i].images.original_still.url);
+                animalImage.attr("data-animate", response.data[i].images.original.url);
+            
+                $("#animalGif").prepend(animalImage);
+                };
+                $("#submit").empty("");
+                
+            
+            
+            $(".gif").on("click", function() {
+                var state = $(this).attr("data-state");
+                if (state === "animate") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "still");
+                } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "animate");
+                }
+            });
+        
+            });
     });
- 
-
-// $(".gif").on("click", function() {
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-    // var state = $(this).attr("data-state");
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
-    // if (state === "still") {
-    //   $(this).attr("src", $(this).attr("data-animate"));
-    //   $(this).attr("data-state", "animate");
-    // } else {
-    //   $(this).attr("src", $(this).attr("data-still"));
-    //   $(this).attr("data-state", "still");
-    // }
-//   });
-
-});
-
+};
 renderButtons();
